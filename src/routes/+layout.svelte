aba1627aa31edead8c6694175254190bd540e801
Raul Maulidhino <raulahmadm314@gmail.com>
2025-08-31 13:01:15 +0700

    feat: add main layout for every page in the website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import Header from './components/Header.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-{@render children?.()}
+<div>
+	<Header />
+	<main>
+		{@render children?.()}
+	</main>
+</div>
