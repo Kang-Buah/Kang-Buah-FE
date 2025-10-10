c4f694aeac06e9e8d870cabf10d6e96135658738
raulmaulidhino-dev <raulahmadm314@gmail.com>
2025-10-10 22:46:39 +0700

    feat: add Home page by combining all components related to Home or Landing Page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Hero from './components/Hero.svelte';
+	import WhyChooseUs from './components/WhyChooseUs.svelte';
+	import ProductVariations from './components/ProductVariations.svelte';
+	import OurProducts from './components/OurProducts.svelte';
+	import DealOfTheDay from './components/DealOfTheDay.svelte';
+</script>
+
+<Hero />
+<WhyChooseUs />
+<ProductVariations />
+<OurProducts />
+<DealOfTheDay />
